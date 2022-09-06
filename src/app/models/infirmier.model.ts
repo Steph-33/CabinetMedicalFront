7952edbfa976746ae460ff8d19534884218eb8f3
Axel CALTE <calteaxelc@gmail.com>
2022-09-06 16:58:27 +0200

    Add services, models, components packages

---
+++ src/app/models/infirmier.model.ts
@@ -0,0 +1,8 @@
+export class Infirmier {
+    public id?: string;
+    public nom?: string;
+    public prenom?: string;
+    public tel_pro?: string;
+    public tel_mobile?: string;
+    public tel_fixe?: string;
+}
