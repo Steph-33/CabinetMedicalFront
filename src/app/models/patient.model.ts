42866a3e8c3d81b95b61ae08e7f3016065fc6363
Steph MANESCHI <stephanie.maneschi@hotmail.fr>
2022-09-09 08:58:20 +0200

    Add a nurse to a patient. Use ActivatedRoute to give parameters in the url

--- src/app/models/patient.model.ts
+++ src/app/models/patient.model.ts
@@ -1,3 +1,5 @@
+import { Infirmier } from "./infirmier.model";
+
 export class Patient {
     public id?: string;
     public nomPatient?: string;
@@ -7,17 +9,6 @@ export class Patient {
     public adresse?: string;
     public numeroSecu?: string;
     public active?: boolean;
+    public infirmier?: Infirmier;
 
-    /*
-    public constructor(nomPatient: string, prenomPatient: string, dateNaissance: string, sexe: string,
-        adresse: string, numeroSecu: string, active: boolean) {
-        this.nomPatient = nomPatient;
-        this.prenomPatient = prenomPatient;
-        this.dateNaissance = dateNaissance;
-        this.sexe = sexe;
-        this.adresse = adresse;
-        this.numeroSecu = numeroSecu;
-        this.active = active;
-    }
-    */
 }
