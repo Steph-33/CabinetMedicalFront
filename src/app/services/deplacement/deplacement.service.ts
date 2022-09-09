c4ccbcc523cd9ce43c2a00c7435a50bb0fc23361
Stéphane PATERNA <paterna@club-internet.fr>
2022-09-09 14:41:51 +0200

    Add choose deplacements by patients and infirmiers names

--- src/app/services/deplacement/deplacement.service.ts
+++ src/app/services/deplacement/deplacement.service.ts
@@ -14,37 +14,44 @@ export class DeplacementService {
   constructor(private http: HttpClient) { }
 
   /**
+  * Get All Deplacements
+  */
+  getAllDeplacements: () => Observable<Deplacement[]> = () => {
+    return this.http.get<Deplacement[]>(`${this.domaine}/deplacements`);
+  }
+
+  /**
   * Get Deplacement by patient 
   */
-  getDeplacementByPatient : (idPatient: string) => Observable<Deplacement[]> = (idPatient: string) => {
+  getDeplacementByPatient: (idPatient: string) => Observable<Deplacement[]> = (idPatient: string) => {
     return this.http.get<Deplacement[]>(`${this.domaine}/deplacements/${idPatient}`);
   }
 
   /**
   * Get Deplacement by patient and infirmier 
   */
-   getDeplacementByPatientAndInfirmier : (idPatient: string, idInfirmier : string) => Observable<Deplacement[]> = (idPatient: string, idInfirmier : string) => {
+  getDeplacementByPatientAndInfirmier: (idPatient: string, idInfirmier: string) => Observable<Deplacement[]> = (idPatient: string, idInfirmier: string) => {
     return this.http.get<Deplacement[]>(`${this.domaine}/deplacements/${idPatient}/${idInfirmier}`);
   }
 
   /**
   * Create deplacement  
   */
-  createDeplacement : (item: Deplacement) => Observable<Observable<Deplacement>> = (item: Deplacement) => {
+  createDeplacement: (item: Deplacement) => Observable<Observable<Deplacement>> = (item: Deplacement) => {
     return this.http.post<Observable<Deplacement>>(`${this.domaine}/deplacements`, item);
   }
 
   /**
   * Update deplacement  
   */
-   updateDeplacement : (id : string, item: Deplacement) => Observable<Observable<Deplacement>> = (id : string, item: Deplacement) => {
+  updateDeplacement: (id: string, item: Deplacement) => Observable<Observable<Deplacement>> = (id: string, item: Deplacement) => {
     return this.http.patch<Observable<Deplacement>>(`${this.domaine}/deplacements/${id}`, item);
   }
 
   /**
    *  Delete deplacement
    */
-   deleteDeplacement : (id: string|undefined) => Observable<Deplacement> = (id: string|undefined) => {
+  deleteDeplacement: (id: string | undefined) => Observable<Deplacement> = (id: string | undefined) => {
     return this.http.delete<Deplacement>(`${this.domaine}/deplacements/${id}`);
   }
 
